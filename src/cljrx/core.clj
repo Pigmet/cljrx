3a7f557f6b0b27befe44bad9204a959a239bb8d7
Gou <nakamura462645@gmail.com>
2020-04-29 20:10:19 +0900

    added rx etc

--- src/cljrx/core.clj
+++ src/cljrx/core.clj
@@ -1,6 +1,56 @@
-(ns cljrx.core)
+(ns cljrx.core
+  (:require [clojure.string :as st])
+  (:refer-clojure :exclude [+ *]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; should make this a library. 
+
+(def word "\\w")
+
+(def non-word "\\W")
+
+(def whitespace "\\s")
+
+(def non-whitespace "\\S")
+
+(def number "\\d")
+
+(def non-number "\\D")
+
+(def lower "\\p{Lower}")
+
+(def upper "\\p{Upper}")
+
+(def alpha "\\p{Alpha}")
+
+(def alnum "\\p{Alnum}")
+
+(def punct "\\p{Punct}")
+
+(def graph "\\p{Graph}")
+
+(def anything ".")
+
+(def word-boundary "\\b")
+
+(def non-word-boundary "\\B")
+
+(def end-of-line "$")
+
+(def beginning-of-line "^")
+
+(def dot "\\.")
+
+(defn re [& args] (re-pattern (apply str args)))
+
+(defn + [& args] (re-pattern (str  (apply str args) "+")))
+
+(defn * [& args] (re-pattern (str  (apply str args) "*")))
+
+(defn ? [& args] (re-pattern (str  (apply str args) "?")))
+
+
+
+
+
+
+
